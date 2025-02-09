004c0c6598f6b6dce29fdfbda2da40d7c963c2b9
ChristyCodes66 <c.adekunle@aol.com>
2025-02-09 13:34:11 +0100

    Include JSX expressions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import MyButton from './MyButton'
 
+const user = 'Alex';
+
 function App() {
   return (
     <>
       <h1>Welcome to my React App!</h1>
       <MyButton />
+      <h1>Hello, {user}!</h1>
+      <p>The year is currently {new Date().getFullYear()}.</p>
     </>
   )
 }
