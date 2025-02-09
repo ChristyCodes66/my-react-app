733b356b57f8f19d0fb17467937a19ea3937407d
ChristyCodes66 <c.adekunle@aol.com>
2025-02-09 14:47:16 +0100

    Modify App to include state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css'
 import MyButton from './MyButton'
+import { useState } from 'react';
 
 const user = 'Alex';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <h1>Welcome to my React App!</h1>
       <MyButton />
       <h1>Hello, {user}!</h1>
       <p>The year is currently {new Date().getFullYear()}.</p>
+      <div>
+        <h1>Counter: {count}</h1>
+        <button onClick={() => setCount(count +1)}>Increase</button>
+      </div>
     </>
   )
 }
