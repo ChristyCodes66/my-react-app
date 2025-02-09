9e18cf2816b6a8e2f01c7cb99f4b576d2e487409
ChristyCodes66 <c.adekunle@aol.com>
2025-02-09 17:16:10 +0100

    Comment out last function - uncomment to see function

--- src/App.jsx
+++ src/App.jsx
@@ -10,34 +10,34 @@ function App() {
   const [show, setShow] = useState(true);
   const [data, setData] = useState(null);
 
-  // return (
-    // <>
-    //   <h1>Welcome to my React App!</h1>
-    //   <MyButton label={'Click Me'}/>
-    //   <h1>Hello, {user}!</h1>
-    //   <p>The year is currently {new Date().getFullYear()}.</p>
-    //   <div>
-    //     <h1>Counter: {count}</h1>
-    //     <button onClick={() => setCount(count +1)}>Increase</button>
-    //   </div>
-    //   <div>
-    //     <MyButton label='Press Me'/>
-    //     <MyButton label={'Don\'t press me'} />
-    //   </div>
-    //   <div>
-    //     <ul>
-    //       {items.map((item, index) => (
-    //         <li key={index}>{item}</li>
-    //       ))}
-    //     </ul>
-    //   </div>
-    //   <div>
-    //     <button onClick={() => setShow(!show)}>
-    //       {show ? "Hide" : "Show"} Message
-    //     </button>
-    //     {show && <p>This is a secret message!</p>}
-    //   </div>
-    //   <div>
+  return (
+    <>
+      <h1>Welcome to my React App!</h1>
+      <MyButton label={'Click Me'}/>
+      <h1>Hello, {user}!</h1>
+      <p>The year is currently {new Date().getFullYear()}.</p>
+      <div>
+        <h1>Counter: {count}</h1>
+        <button onClick={() => setCount(count +1)}>Increase</button>
+      </div>
+      <div>
+        <MyButton label='Press Me'/>
+        <MyButton label={'Don\'t press me'} />
+      </div>
+      <div>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <button onClick={() => setShow(!show)}>
+          {show ? "Hide" : "Show"} Message
+        </button>
+        {show && <p>This is a secret message!</p>}
+      </div>
+    {/*  <div>
         useEffect(() => {
           fetch("https://jsonplaceholder.typicode.com/todos/1")
           .then((response) => response.json())
@@ -45,9 +45,9 @@ function App() {
         }, []);
 
         return <pre>{JSON.stringify(data, null, 2)}</pre>
-  //     </div>
-  //   </>
-  // );
+      </div> */}
+    </>
+  );
 }
 
 export default App
